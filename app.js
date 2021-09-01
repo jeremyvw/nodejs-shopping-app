fe1fa175e35b59fbe4c0b8a936f69be3babedf8c
Jeremy Vijay Wongso <jeremyvw30@gmail.com>
2021-09-02 00:04:27 +0700

    Finish up the controller

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 // const expressHbs = require('express-handlebars'); // Handlebars Template Engine
 const app = express();
 
@@ -27,8 +29,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
