cb7b12ffaf111f0ccbc603431f6239472059f360
Jeremy Vijay Wongso <jeremyvw30@gmail.com>
2021-09-01 20:59:40 +0700

    Convert HTML to EJS

--- app.js
+++ app.js
@@ -2,20 +2,20 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
-
+// const expressHbs = require('express-handlebars'); // Handlebars Template Engine
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-);
-// app.set('view engine', 'pug');
-app.set('view engine', 'hbs');
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs',
+//   })
+// ); // Handlebars Template Engine
+
+// app.set('view engine', 'pug'); // Pug Template Engine
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
