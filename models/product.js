a6ba84d6c90d5e53a84559c6289ecdf00d2f73fa
Jeremy Vijay Wongso <jeremyvw30@gmail.com>
2021-09-04 01:06:08 +0700

    Insert data into the database

--- models/product.js
+++ models/product.js
@@ -10,7 +10,12 @@ module.exports = class Product {
     this.description = description;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      'INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.imageUrl, this.price, this.description]
+    );
+  }
 
   static deleteById(id) {}
 
