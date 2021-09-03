87160d0500f97a8fbf85da69868afb12e1c7c4e6
Jeremy Vijay Wongso <jeremyvw30@gmail.com>
2021-09-04 06:07:03 +0700

    Reset the cart & fetch and output order

--- controllers/shop.js
+++ controllers/shop.js
@@ -129,9 +129,10 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  req.user
+  let fetchedCart = req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -150,6 +151,9 @@ exports.postOrder = (req, res, next) => {
         });
     })
     .then((result) => {
+      fetchedCart.setProducts(null);
+    })
+    .then((result) => {
       res.redirect('/orders');
     })
     .catch((err) => {
@@ -158,15 +162,16 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout',
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
