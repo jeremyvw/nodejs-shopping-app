9113183028ccd840459280ba5710e10bec1edfef
Jeremy Vijay Wongso <jeremyvw30@gmail.com>
2021-09-04 04:05:40 +0700

    Define one-to-many & many-to-many relations

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 // const expressHbs = require('express-handlebars'); // Handlebars Template Engine
 const app = express();
 
@@ -47,6 +49,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   // .sync({ force: true })
